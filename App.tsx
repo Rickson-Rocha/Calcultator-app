e3e8b745054f405beea606962f242291d6fc3108
Roberto Rickson Câmara Rocha <20221148060011@ifrn.local>
2024-07-16 11:52:20 -0300

    [feature]: add  button component and themeContext

--- App.tsx
+++ App.tsx
@@ -1,19 +1,33 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Switch } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { myColors } from './src/styles/Colors';
+import Button from './src/components/Button';
 export default function App() {
+  const [theme,setTheme] = useState('light');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <ThemeContext.Provider value='{theme}'>
+    <View style={theme === 'light' ? styles.container : [styles.container, { backgroundColor: '#000' }]}>
       <StatusBar style="auto" />
+      <Switch
+         value = {theme === 'light'}
+         onValueChange ={() => setTheme(theme == 'light' ? 'dark' : 'light')}
+        /> 
+    <Button isBlue title='3' onPress={function (): void {
+          throw new Error('Function not implemented.');
+        } }/>
     </View>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
     justifyContent: 'center',
   },
