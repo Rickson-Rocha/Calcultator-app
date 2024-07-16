3a3059adf7afa23914d27c62e638a31adec564f2
Roberto Rickson Câmara Rocha <20221148060011@ifrn.local>
2024-07-16 11:49:25 -0300

    [feature]: add component keyboard

---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,21 @@
+// import * as React from "react";
+// import Button from "./Button";
+// import {View,Text} from "react-native";
+// import { Styles } from "../styles/GlobalStyles";
+// import { myColors } from "../styles/Colors";
+
+// export default function MyKeyboard(){
+//  const [firstNumber, setFirsNumber] =  React.useState("");
+//  const [secondNumber, setsecondNumber] =  React.useState("");
+//  const [operation, setOperation] = React.useState("");
+//  const [result, setResult] = React.useState<Number | null >(null);
+//  const handleNumberPress = (buttonValue:string) =>{
+//     if (firstNumber.length < 10){
+//         setFirsNumber(firstNumber + buttonValue);
+//     }
+//  }
+//  return(
+
+//  )
+
+// }
